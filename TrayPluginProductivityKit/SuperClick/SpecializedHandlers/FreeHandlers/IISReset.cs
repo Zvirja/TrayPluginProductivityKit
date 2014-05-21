cd1b41ce3360369ea36f7f123cd2bf63c755a59c
Alex Povar <zvirja1@gmail.com>
2014-05-21 19:24:46 +0300

    Found that sometimes fast iis reset doesn't work because it cannot terminate exisiting processes. Added code to kill them before.

--- TrayPluginProductivityKit/SuperClick/SpecializedHandlers/FreeHandlers/IISReset.cs
+++ TrayPluginProductivityKit/SuperClick/SpecializedHandlers/FreeHandlers/IISReset.cs
@@ -12,6 +12,13 @@ namespace TrayPluginProductivityKit.SuperClick.SpecializedHandlers.FreeHandlers
   {
     protected override bool ProcessClickInternal(ClickDetailsWrapper clickDetails)
     {
+      //Kill all w3wp processes before.
+      Process[] w3wpProcesses = Process.GetProcessesByName("w3wp");
+      foreach (Process w3wpProcess in w3wpProcesses)
+      {
+        w3wpProcess.Kill();
+      }
+
       var param = "/timeout:0";
       var psi = new ProcessStartInfo("iisreset", param);
       Process.Start(psi);
